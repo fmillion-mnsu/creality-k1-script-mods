39e9e9dcbeb7900bb9d7426a01979c743a14ae43
fmillion <fmillion@gmail.com>
2024-05-06 07:59:44 +0000

    Comments in install script

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# This will install the script files to a rooted K1.
+
+# You either need to provide your K1's root password THREE times,
+# or you can make it better by adding your machine's public SSH
+# key to the authorized_keys file in /root/.ssh on the K1.
+
 K1_IP_ADDR=192.168.32.253
 
 scp custom_macro.py root@${K1_IP_ADDR}:/usr/share/klipper/klippy/extras
